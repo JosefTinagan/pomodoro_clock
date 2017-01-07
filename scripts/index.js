70202b8c4987d3302c419436ac6995ef8a81a662
Josef Perry Tinagan <josephperry_06@yahoo.com>
2017-01-07 16:38:57 +0800

    added start_timer function

--- scripts/index.js
+++ scripts/index.js
@@ -6,28 +6,49 @@ function increase_session(){
   var elem, num;
 	elem = document.querySelector("#session_number");
  	num = parseInt(elem.innerHTML,10)
-	document.querySelector("#session_number").innerHTML = num + 1;
+ 	num = num + 1;
+	document.querySelector("#session_number").innerHTML = num;
+	 if(num < 10){
+  		str = "0" + num + ":" + "00";
+	 } else {
+	  	str = num + ":" + "00";
+	 }
+	 document.querySelector("#timer").innerHTML = str;
 }
 
 function decrease_session(){
 	var elem, num;
   elem = document.querySelector("#session_number");
   num = parseInt(elem.innerHTML,10);
-  document.querySelector("#session_number").innerHTML = num - 1;
+  num = num - 1;
+  document.querySelector("#session_number").innerHTML = num ;
+  if(num < 10){
+  		str = "0" + num + ":" + "00";
+	 } else {
+	  	str = num + ":" + "00";
+	 }
+	 document.querySelector("#timer").innerHTML = str;
 }
 
 function increase_break(){
 	var elem, num;
   elem = document.querySelector("#break_number");
   num = parseInt(elem.innerHTML,10);
-  document.querySelector("#break_number").innerHTML = num + 1;
+  num = num + 1;
+  document.querySelector("#break_number").innerHTML = num;
 }
 
 function decrease_break(){
-	var elem, num;
+	var elem, num, str;
   elem = document.querySelector("#break_number");
   num = parseInt(elem.innerHTML,10);
-  document.querySelector("#break_number").innerHTML = num - 1;
+  num = num - 1;
+  document.querySelector("#break_number").innerHTML = num;
+
 }
 
-
+function start_timer(){
+	elem = document.querySelector("#timer");
+	num = parseInt(elem.innerHTML,10);
+	
+}
