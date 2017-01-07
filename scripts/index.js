cfb8a74aadcd339b2f518febaa9b710905860d21
Josef Perry Tinagan <josefperrytinagan@gmail.com>
2017-01-07 15:29:17 +0800

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,63 +1,33 @@
-
 function test(){
-	timer = new Date();
-	timer.setMinutes(45);
-	timer = timer.getMinutes();
-	document.getElementById("test").innerHTML = "BOOMMMMM" + timer;
+	document.querySelector("#test").innerHTML = "oooop";
 }
 
-function startTimer(duration, display) {
-	var timer = duration, minutes, seconds;
-	setInterval(function () {
-		minutes = parseInt(timer / 60, 10);
-		seconds = parseInt(timer % 60, 10);
-
-		minutes = minutes < 10 ? "0" + minutes : minutes;
-		seconds = seconds < 10 ? "0" + seconds : seconds;
-
-		display.textContent = minutes + ":" + seconds;
-
-		if (--timer < 0) {
-			timer = duration;
-		}
-	}, 1000);
+function increase_session(){
+  var elem, num;
+	elem = document.querySelector("#session_number");
+ 	num = parseInt(elem.innerHTML,10)
+	document.querySelector("#session_number").innerHTML = num + 1;
 }
 
-function beginTimer(){
-	var fiveMinutes = 60 * 5,
-		display = document.querySelector('#time');
-		startTimer(fiveMinutes, display);
+function decrease_session(){
+	var elem, num;
+  elem = document.querySelector("#session_number");
+  num = parseInt(elem.innerHTML,10);
+  document.querySelector("#session_number").innerHTML = num - 1;
 }
 
-/*
-window.onload = function() {
-	var fiveMinutes = 60 * 5,
-		display = document.querySelector('#time');
-	startTimer(fiveMinutes, display);
-
-};
-
-/*
-function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10)
-        seconds = parseInt(timer % 60, 10);
-
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.textContent = minutes + ":" + seconds;
-
-        if (--timer < 0) {
-            timer = duration;
-        }
-    }, 1000);
+function increase_break(){
+	var elem, num;
+  elem = document.querySelector("#break_number");
+  num = parseInt(elem.innerHTML,10);
+  document.querySelector("#break_number").innerHTML = num + 1;
 }
 
-window.onload = function () {
-    var fiveMinutes = 60 * 5,
-        display = document.querySelector('#time');
-    startTimer(fiveMinutes, display);
-};
-*/
+function decrease_break(){
+	var elem, num;
+  elem = document.querySelector("#break_number");
+  num = parseInt(elem.innerHTML,10);
+  document.querySelector("#break_number").innerHTML = num - 1;
+}
+
+
